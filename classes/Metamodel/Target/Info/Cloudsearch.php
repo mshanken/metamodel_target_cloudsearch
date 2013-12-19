2850046a6797e7fc38dc6031b3d8e3a0a0a835b7
chandeeland <chand@chandeeland.org>
2013-12-19 12:41:54 -0500

    better validator in target_info classes

--- classes/Metamodel/Target/Info/Cloudsearch.php
+++ classes/Metamodel/Target/Info/Cloudsearch.php
@@ -15,20 +15,51 @@
  * @subpackage Target_Info
  * @author dchan@mshanken.com
  */
-Class Metamodel_Target_Info_Cloudsearch
-extends Target_Info
+class Metamodel_Target_Info_Cloudsearch extends Target_Info
 {
+    /**
+     * entity_name 
+     *
+     * @var mixed
+     * @access private
+     */
     private $entity_name = NULL;
+    
+    /**
+     * field_types 
+     *
+     * @var array
+     * @access private
+     */
     private $field_types = array();
+
+    /**
+     * facet_maps 
+     *
+     * @var array
+     * @access private
+     */
     private $facet_maps = array();
     
+    /**
+     * Constructor
+     *
+     * @access public
+     */
     public function __construct() 
     {
     }
     
-    // @TODO
+    /**
+     * validate
+     *
+     * @param Entity_Root $root
+     * @access public
+     * @return void
+     */
     public function validate(Entity_Root $root)
     {
+        if (!($root instanceof Target_Cloudsearchable)) return false;
         if(!($root[Target_Cloudsearch::VIEW_PAYLOAD] instanceof Entity_Columnset)) return false;
         if(!($root[Target_Cloudsearch::VIEW_INDEXER] instanceof Entity_Columnset)) return false;
         
